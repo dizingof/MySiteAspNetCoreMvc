6bc6ca51a5ded67d5ef2703babd8bf9210361fc6
Roman.Nepomniashchyi <Roman.Nepomniashchyi@infopulse.com>
2020-08-11 00:50:07 +0300

    add service controller

--- MySiteAspNetCoreMvc/Controllers/HomeController.cs
+++ MySiteAspNetCoreMvc/Controllers/HomeController.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MySiteAspNetCoreMvc.Domain;
 
 namespace MySiteAspNetCoreMvc.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly DataManager dataManager;
+
+        public HomeController(DataManager dataManager)
+        {
+            this.dataManager = dataManager;
+        }
         public IActionResult Index()
         {
-            return View();
+            return View(dataManager.TextFields.GetTextFieldByCodeWord("PageIndex"));
+        }
+        public IActionResult Contacts()
+        {
+            return View(dataManager.TextFields.GetTextFieldByCodeWord("PageContacts"));
         }
     }
 }
